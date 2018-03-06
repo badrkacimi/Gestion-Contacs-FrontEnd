c3500112f0e76adc4838ea2661f113d3646a4c4f
Badr Kacimi <badrvkacimi@gamail.com>
2018-03-06 11:44:37 +0000

    badr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {RouterModule,Routes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import {FormsModule} from '@angular/forms';
+import { AboutComponent } from './about/about.component';
+import { HttpClientModule } from '@angular/common/http';
+import {ContactService} from '../services/contacts.service';
+import { NewContactComponent } from './new-contact/new-contact.component';
+import { NouveauContactComponent } from './nouveau-contact/nouveau-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
 
 
+const appRoutes : Routes=[
+  {path : 'contacts', component : ContactsComponent},
+  {path : 'about', component : AboutComponent},
+  {path : 'nouveau-contact', component : NouveauContactComponent},
+  {path : 'edit-contact/:id', component : EditContactComponent},
+  {path : '',
+    redirectTo : '/about',
+    pathMatch : 'full'},
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    AboutComponent,
+    NewContactComponent,
+    NouveauContactComponent,
+    EditContactComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ContactService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
